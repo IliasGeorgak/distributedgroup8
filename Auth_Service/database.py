38307d6e054e8bfb31d255098e22ca9c81017478
Ilias Georgakopoulos <igeorgakopoulos@tuc.gr>
2026-04-25 19:55:54 +0300

    UI + AUTH + POSTGRES all now live on the cluster

--- Auth_Service/database.py
+++ Auth_Service/database.py
@@ -2,6 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
+import base64
 
 # db_pass = "Aggelos123@"
 # db_port = "5432"
@@ -9,7 +10,8 @@ host = os.environ["POSTGRES_HOST"]
 port = os.environ["POSTGRES_PORT"]
 user = os.environ["POSTGRES_USER"]
 db = os.environ["POSTGRES_DB"]
-password = os.environ["POSTGRES_PASSWORD"]
+password = os.environ["POSTGRES_PASSWORD"].strip()
+#password = base64.b64decode(password.strip()).decode("utf-8")
 SQLALCHEMY_DATABASE_URL = f"postgresql+psycopg2://{user}:{password}@{host}:{port}/{db}"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
