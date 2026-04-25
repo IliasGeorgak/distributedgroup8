8bd066b231216674bea461e4ee0105219826df37
SabbathBro <acdcaggelos@gmail.com>
2026-04-25 23:03:52 +0300

    Fixes some database connection issues

--- Auth_Service/config.py
+++ Auth_Service/config.py
@@ -1,9 +1,15 @@
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    SECRET_KEY: str = "supersecretkey"
+    SECRET_KEY: str 
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+
+    POSTGRES_HOST: str
+    POSTGRES_PORT: int
+    POSTGRES_USER: str
+    POSTGRES_DB: str
+    POSTGRES_PASSWORD: str
 
     class Config:
         env_file = ".env"
